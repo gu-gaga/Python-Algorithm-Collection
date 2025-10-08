11f2a931417b48698568e4396caf57ddc9cea957
gu-gaga <3263246663@qq.com>
2025-10-08 12:12:48 +0800

    Add files via upload

---
+++ quick_select.py
@@ -0,0 +1,31 @@
+# 快速选择
+def quick_sort(arr, l, r,k):
+    if l==r:
+        return arr[l]
+    mid = arr[l]
+    i,j=l,r
+    while i < j:
+        while i < j and arr[j]>mid:
+            j-=1
+        if i<j:
+            arr[i]=arr[j]
+            i+=1
+        while i < j and arr[i]<=mid:
+            i+=1
+        if i<j:
+            arr[j]=arr[i]
+            j-=1
+    arr[i]=mid
+    if k-1==i:
+        return arr[i]
+    elif k-1<i:
+        return quick_sort(arr, l, i-1,k)
+    else:
+        return quick_sort(arr, i+1, r,k)
+
+
+print("请输入数列，用空格分隔：")
+arr = [int(x) for x in input().split()]
+print("请输入k：")
+k=int(input())
+print(quick_sort(arr, 0, len(arr) - 1,k))
