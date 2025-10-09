aa4d3fc77c7a347ae78a2ca5b63d6e363cae2a79
sususuper <3263246663@qq.com>
2025-10-10 00:26:08 +0800

    Add files via upload

---
+++ basic algorithm/sum_of_submatrices.py
@@ -0,0 +1,14 @@
+# 子矩阵的和（二维前缀和）
+import numpy as np
+n,m,q=map(int,input().split())
+matrix=np.zeros((n+1,m+1))
+S=np.zeros((n+1,m+1))
+print(S)
+for i in range(1,n+1):
+    matrix[i][1:] = list(map(int,input().split()))
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        S[i][j]=S[i-1][j]+S[i][j-1]-S[i-1][j-1]+matrix[i][j]
+for i in range(q):
+    x1,y1,x2,y2=map(int,input().split())
+    print(S[x2][y2]-S[x1-1][y2]-S[x2][y1-1]+S[x1-1][y1-1])
