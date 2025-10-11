4ed3357bf5c787d1aed3eabe482fcdfb6d2f846f
sususuper <3263246663@qq.com>
2025-10-11 12:03:24 +0800

    Add files via upload

---
+++ basic algorithm/target_sum_of_array_elements.py
@@ -0,0 +1,11 @@
+# 数组元素的目标和（双指针）
+n,m,x=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+j=m-1
+for i in range(n):
+    while a[i]+b[j]>x:
+        j-=1
+    if a[i]+b[j]==x:
+        print(f"{a[i]} {b[j]}")
+        break
