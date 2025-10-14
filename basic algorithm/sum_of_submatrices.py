9a218001b31454deed16a96f72aad83228acb295
sususuper <3263246663@qq.com>
2025-10-14 16:24:36 +0800

    Update sum_of_submatrices.py

--- basic algorithm/sum_of_submatrices.py
+++ basic algorithm/sum_of_submatrices.py
@@ -3,7 +3,6 @@ import numpy as np
 n,m,q=map(int,input().split())
 matrix=np.zeros((n+1,m+1),dtype=int)
 S=np.zeros((n+1,m+1),dtype=int)
-print(S)
 for i in range(1,n+1):
     matrix[i][1:] = list(map(int,input().split()))
 for i in range(1,n+1):
@@ -12,3 +11,4 @@ for i in range(1,n+1):
 for i in range(q):
     x1,y1,x2,y2=map(int,input().split())
     print(S[x2][y2]-S[x1-1][y2]-S[x2][y1-1]+S[x1-1][y1-1])
+
