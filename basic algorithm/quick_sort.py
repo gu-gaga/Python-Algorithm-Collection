123f8e8fd5785555769978b8835d65c68546afc6
sususuper <3263246663@qq.com>
2025-10-14 21:46:02 +0800

    Update quick_sort.py

--- basic algorithm/quick_sort.py
+++ basic algorithm/quick_sort.py
@@ -1,26 +1,24 @@
 # 快速排序
 def quick_sort(arr, l, r):
-    if l>=r:
+    if l >= r:
         return
-    mid = arr[l]
-    i,j=l,r
-    while i < j:
-        while i < j and arr[j]>mid:
-            j-=1
-        if i<j:
-            arr[i]=arr[j]
-            i+=1
-        while i < j and arr[i]<=mid:
-            i+=1
-        if i<j:
-            arr[j]=arr[i]
-            j-=1
-    arr[i]=mid
-    quick_sort(arr, l, i-1)
-    quick_sort(arr, i+1, r)
+    pivot = arr[(l + r) // 2]
+    i, j = l, r
+    while i <= j:
+        while arr[i] < pivot:
+            i += 1
+        while arr[j] > pivot:
+            j -= 1
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+    if l < j:
+        quick_sort(arr, l, j)
+    if i < r:
+        quick_sort(arr, i, r)
 
-
-print("请输入要排序的数字，用空格分隔：")
-arr = [int(x) for x in input().split()]
-quick_sort(arr, 0, len(arr) - 1)
-print(arr)
+n=int(input())
+arr=list(map(int,input().split()))
+quick_sort(arr,0,n-1)
+print(*arr)
